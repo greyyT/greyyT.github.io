f5d88eb34d11a59738e34212c90d2a4e73766d95
greyyT <nhannguyen8338@gmail.com>
2023-10-16 20:14:00 +0700

    Add familiar technologies

--- src/components/Tech.tsx
+++ src/components/Tech.tsx
@@ -1,7 +1,17 @@
+import { technologies } from '@/constants';
 import { SectionWrapper } from '@/hoc';
+import { BallCanvas } from '.';
 
 const TechPage = () => {
-  return <div className="">Tech</div>;
+  return (
+    <div className="flex flex-wrap justify-center gap-10">
+      {technologies.map((technology, idx: number) => (
+        <div className="w-28 h-28" key={idx}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 const Tech = SectionWrapper(TechPage, '');
--- src/components/canvas/BallCanvas.tsx
+++ src/components/canvas/BallCanvas.tsx
@@ -1,5 +1,38 @@
-const BallCanvas = () => {
-  return <h1></h1>;
+import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { Suspense } from 'react';
+import { CanvasLoader } from '..';
+
+interface BallCanvasProps {
+  icon: string;
+}
+
+const Ball = ({ imgUrl }: { imgUrl: string }) => {
+  const [decal] = useTexture([imgUrl]);
+
+  return (
+    <Float speed={0.75} rotationIntensity={0.5} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0.025, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
+        <Decal position={[0, 0, 1]} map={decal} rotation={[2 * Math.PI, 0, 6.25]} />
+      </mesh>
+    </Float>
+  );
+};
+
+const BallCanvas: React.FC<BallCanvasProps> = ({ icon }) => {
+  return (
+    <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
 };
 
 export default BallCanvas;
