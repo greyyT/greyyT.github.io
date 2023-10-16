980bb2dc097ab6f5a169e84c4e760d8d78dd41c5
greyyT <nhannguyen8338@gmail.com>
2023-10-16 20:21:04 +0700

    Change technology ball to round sphere

--- src/components/canvas/BallCanvas.tsx
+++ src/components/canvas/BallCanvas.tsx
@@ -12,10 +12,10 @@ const Ball = ({ imgUrl }: { imgUrl: string }) => {
 
   return (
     <Float speed={0.75} rotationIntensity={0.5} floatIntensity={2}>
-      <ambientLight intensity={0.25} />
-      <directionalLight position={[0, 0.025, 0.05]} />
+      <ambientLight intensity={0.35} />
+      <directionalLight position={[0, 0.01, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 1]} />
+        <icosahedronGeometry args={[1, 3]} />
         <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
         <Decal position={[0, 0, 1]} map={decal} rotation={[2 * Math.PI, 0, 6.25]} />
       </mesh>
